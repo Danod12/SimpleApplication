41f6bda00c5aa02e90438e1f89427901f1bb1247
Daniel O'Driscoll <daodrisc@tcd.ie>
2022-03-24 12:20:10 +0000

    deploy4

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-sudo kill -9 $(sudo lsof -t -i:3000)
+
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer
-sudo npm install -g http-server
-http-server ./SimpleApplication/ -p 3000
+sudo npm install -g pm2
+pm2 stop all
+pm2 serve ./SimpleApplication/ 3000
